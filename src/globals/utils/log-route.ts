9e41d6691c7413e7f6c213ce44cd4e9992631d3a
vannguyenv12 <npnv.vn1@gmail.com>
2025-04-05 16:40:17 +0700

    log route

---
+++ src/globals/utils/log-route.ts
@@ -0,0 +1,40 @@
+import { Application } from 'express';
+
+export const getAllRoutes = (app: Application): { method: string; path: string }[] => {
+  const routes: { method: string; path: string }[] = [];
+
+  const routerStack = app._router.stack;
+
+  const traverse = (stack: any[], prefix = '') => {
+    stack.forEach((layer: any) => {
+      if (layer.route) {
+        const path = layer.route.path;
+        const methods = Object.keys(layer.route.methods)
+          .filter((method) => layer.route.methods[method])
+          .map((method) => method.toUpperCase());
+
+        methods.forEach((method) => {
+          routes.push({ method, path: `${prefix}${path}` });
+        });
+      } else if (layer.handle && layer.handle.stack) {
+        let currentPrefix = prefix;
+
+        if (layer.regexp) {
+          const match = layer.regexp.toString().match(/^\/\^\\\/(.*?)\\\/\?/);
+          if (match) {
+            currentPrefix = `/${match[1].replace(/\\\//g, '/')}`;
+          }
+        }
+
+        traverse(layer.handle.stack, currentPrefix);
+      }
+    });
+  };
+
+  traverse(routerStack);
+
+  return routes.map((route) => ({
+    method: route.method,
+    path: route.path.replace('/api/v1/', '')
+  }));
+};

cc3ad2a214556b18c947a38203c08647bc8655a9
vannguyenv12 <npnv.vn1@gmail.com>
2025-04-08 19:16:28 +0700

    fix access token

--- src/features/user/routes/auth.route.ts
+++ src/features/user/routes/auth.route.ts
@@ -7,15 +7,15 @@ const authRoute = express.Router();
 
 authRoute.post('/signup', asyncWrapper(authController.signUp));
 authRoute.post('/sign-in', asyncWrapper(authController.signIn));
+authRoute.post('/refresh-token', asyncWrapper(authController.refreshToken));
+authRoute.post('/forgot-password', asyncWrapper(authController.forgotPassword));
+authRoute.post('/reset-password', asyncWrapper(authController.resetPassword));
 
 authRoute.use(authMiddleware.verifyUser); // authentication
 
 authRoute.get('/me', authMiddleware.verifyPermission, asyncWrapper(authController.getCurrentUser));
 authRoute.post('/logout', authMiddleware.verifyPermission, asyncWrapper(authController.logout));
 authRoute.get('/protected', authMiddleware.verifyPermission, asyncWrapper(authController.protected));
-authRoute.post('/refresh-token', authMiddleware.verifyPermission, asyncWrapper(authController.refreshToken));
-authRoute.post('/forgot-password', authMiddleware.verifyPermission, asyncWrapper(authController.forgotPassword));
-authRoute.post('/reset-password', authMiddleware.verifyPermission, asyncWrapper(authController.resetPassword));
 authRoute.put('/update-profile', authMiddleware.verifyPermission, asyncWrapper(authController.updateProfile));
 
 export default authRoute;
